e745f94ad3c6a5e822b9d28aeb50e8191c8f395d
GiovanaBt <giovana.r.oliveira05@gmail.com>
2026-02-19 14:37:55 -0300

    mudanças 2

--- enviarCadastroEvento.php
+++ enviarCadastroEvento.php
@@ -1,30 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
 <?php
-include 'testeConexao.php';
+include 'Conexao.php';
 
-$idUsuarios = $_POST['id_usuarios'];
+session_start();
+
+
+$idUsuario = $_SESSION['usuario_id'];
 $nomeDoEvento = $_POST['descricao'];
 $rua = $_POST['rua'];
 $bairro = $_POST['bairro'];
 $numero = $_POST['numero'];
 $cidade = $_POST['cidade'];
 $pontoReferencia = $_POST['ponto_referencia'];
-$dataEvento = $_POST['data_evento'];
-$horarioEvento = $_POST['horario_evento'];
+
+$dataInicioEvento = $_POST['data_inicio_evento'];
+$dataFimEvento = $_POST['data_fim_evento'];
+
+$horarioInicioEvento = $_POST['horario_inicio_evento'];
+$horarioFimEvento = $_POST['horario_fim_evento'];
+
 $categorias = $_POST['categorias'];
 $eventoConcluido = $_POST['evento_concluido'];
        
- $sql= "INSERT INTO Eventos_Cadastrados(id_usuarios, descricao, rua, bairro, numero, cidade, ponto_referencia, data_evento, horario_evento, id_categoria, evento_concluido)
-    VALUES ('$idUsuarios','$nomeDoEvento', '$rua', '$bairro', '$numero', '$cidade', '$pontoReferencia', '$dataEvento', '$horarioEvento', '$categorias', '$eventoConcluido')";
-    
-   echo "<script>alert('Cadastro feito com Sucesso!');
-    window.location.href = 'cadastroEvento.php';
-    </script>";
+ $sql= "INSERT INTO Eventos_Cadastrados(id_usuarios, descricao, rua, bairro, numero, cidade, ponto_referencia, data_inicio_evento, data_fim_evento, horario_inicio_evento, horario_fim_evento, id_categoria, evento_concluido)
+    VALUES ('$idUsuario','$nomeDoEvento', '$rua', '$bairro', '$numero', '$cidade', '$pontoReferencia','$dataInicioEvento', '$dataFimEvento', '$horarioInicioEvento', '$horarioFimEvento', '$categorias', '$eventoConcluido')";
+
     
 if ($conexao->query($sql) === TRUE) {
-    echo "<script>alert ('OK ATE AQUI');</script>";
-    echo "Cadastro efetuado com sucesso!<br>"; 
+    echo "Evento Cadastrado com sucesso";
 } else {
-    echo "Erro ao efetuar cadastro" . $conexao->error;
+    // echo "Erro ao efetuar cadastro" . $conexao->error;
+    
 }
 
 $conexao->close();  
