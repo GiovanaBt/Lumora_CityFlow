cd66283cef3bd4f7dff0cd14a0cdaa1306a37eb3
GiovanaBt <giovana.r.oliveira05@gmail.com>
2026-03-13 14:06:45 -0300

    Mapa funcional
    Uso das APIs correta, aparece a minha localização e a localização dos eventos

--- mapa.php
+++ mapa.php
@@ -12,282 +12,267 @@ while($row = $result->fetch_assoc()){
     $eventos[] = $row;
 }
 
-
 session_start();
 ?>
 
-
-
 <!DOCTYPE html>
-
-<html lang="en">
+<html lang="pt-br">
 
 <head>
 
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
+<title>City Flow - Conecte-se à cultura de sua cidade</title>
 
-    <title>City Flow - Concte-se à cultura de sua cidade</title>
+<link rel="stylesheet" href="mapa.css">
+<link rel="shortcut icon" href="imgs/logoCityFlow.webp">
 
-    <link rel="stylesheet" href="index.css">
-
-    <link rel="shortcut icon" href="imgs/logoCityFlow.webp" type="image/x-icon">
-
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
-
-
-
-        <link
-
-        rel="stylesheet"
-
-        href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
-
-    />
-
-
-
-    <style>
-
-    body {
-
-        margin: 0;
-
-        padding: 0;
-
-        overflow: hidden; /* Evita barras de rolagem desnecessárias */
-
-    }
-
-
-
-    #map {
-
-        height: calc(100vh - 80px); /* Ocupa a tela menos a altura do header */
-
-        width: 100%;
-
-        z-index: 1; /* Garante que fique atrás dos menus */
-
-    }
-
-   
-
-    header {
-
-        position: relative;
-
-        z-index: 1000; /* Garante que o menu e submenus fiquem na frente do mapa */
-
-    }
-
-</style>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+<link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"/>
 
 </head>
 
 <body>
 
-
 <header>
 
-    <div class='logo'>
+<div class='logo'>
+    <a href="index.php">
+    <img src="imgs/logoCityFlow.webp">
+    </a>
+</div>
 
-        <a href="index.php">
+<a href="mapa.php">
+    <button class="botaoMapa">MAPA</button>
+</a>
 
-            <img src="imgs/logoCityFlow.webp" alt="logoCityFlow">
+<ul class="menu" id="menu">
 
+    <li><a href="index.php">INÍCIO</a></li>
+    <li><a href="#informacoes">INFORMAÇÕES</a></li>
+
+    <li>
+        <a href="cadastroEvento.php">
+        <i class="fa-solid fa-circle-plus"></i> DIVULGAR EVENTOS
         </a>
+    </li>
 
-    </div>
+<?php if (isset($_SESSION['usuario_id'])): ?>
 
+<li class="perfil">
 
-
-    <a href="mapa.php">
-
-        <button class="botaoMapa">MAPA</button>
-
+    <a href="#">
+    <i class="fa-solid fa-circle-user"></i>
+    <?php echo $_SESSION['nome_usuario'];?>
     </a>
 
+<ul class="submenu">
+    <li><a href="minhaConta.php">Minha conta</a></li>
+    <li><a href="minhaConta.php#favoritos">Favoritos</a></li>
+    <li><a href="minhaConta.php#meusEventos">Meus eventos</a></li>
+    <li><a href="ajuda.php">Central de ajuda</a></li>
+    <li><a href="logout.php">Sair</a></li>
+</ul>
 
+</li>
 
-    <ul class="menu" id="menu">
+<?php else: ?>
 
-        <li><a href="index.php">INÍCIO</a></li>
+<li class="menu-container">
+    <a id="abrirModal" style="cursor:pointer;">
+    <i class="fa-solid fa-right-to-bracket"> ENTRAR</i>
+    </a>
+</li>
 
-        <li><a href="#informacoes">INFORMAÇÕES</a></li>
+<?php endif; ?>
 
-        <li>
+</ul>
 
-            <a href="cadastroEvento.php">
-
-                <i class="fa-solid fa-circle-plus"></i> DIVULGAR EVENTOS
-
-            </a>
-
-        </li>
-
-
-
-        <?php if (isset($_SESSION['usuario_id'])): ?>
-
-            <li class="perfil">
-
-                <a href="#">
-
-                    <i class="fa-solid fa-circle-user"></i>
-
-                    <?php echo $_SESSION['nome_usuario'];?>
-
-                </a>
-
-                <ul class="submenu">
-
-                    <li><a href="minhaConta.php">Minha conta</a></li>
-
-                    <li><a href="minhaConta.php#favoritos">Favoritos</a></li>
-
-                    <li><a href="minhaConta.php#meusEventos">Meus eventos</a></li>
-
-                    <li><a href="ajuda.php">Central de ajuda</a></li>
-
-                    <li><a href="logout.php">Sair</a></li>
-
-                </ul>
-
-            </li>
-
-        <?php else: ?>
-
-            <li class="menu-container">
-
-                <a id="abrirModal" style="cursor: pointer;">
-
-                    <i class="fa-solid fa-right-to-bracket"> ENTRAR</i>
-
-                </a>
-
-            </li>
-
-        <?php endif; ?>
-
-    </ul>
-
-
-
-    <div class="hamburguer" id="hamburguer">
-
-        <i class="fa-solid fa-bars"></i>
-
-    </div>
+<div class="hamburguer" id="hamburguer">
+    <i class="fa-solid fa-bars"></i>
+</div>
 
 </header>
 
 <div id="map"></div>
 
-
-
-<!-- JS do Leaflet -->
+<button id="btnLocalizacao">📍 Minha localização</button>
 
 <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"></script>
 
-
-
 <script>
 
-    // Criar o mapa (posição inicial padrão - Brasil)
+// mapa 
 
-    var map = L.map('map').setView([-23.5505, -46.6333], 13);
+var map = L.map('map').setView([-14.2350,-51.9253],4); // Brasil inicial
 
-
-
-    // Adicionar camada do OpenStreetMap
-
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-
-        attribution: '&copy; OpenStreetMap contributors'
-
-    }).addTo(map);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
+attribution:'&copy; OpenStreetMap'
+}).addTo(map);
 
 
+//icones 
 
-    // Verificar se o navegador suporta geolocalização
+var iconeUsuario = L.icon({
+iconUrl:"https://cdn-icons-png.flaticon.com/512/64/64113.png",
+iconSize:[32,32],
+iconAnchor:[16,32]
+});
 
-    if (navigator.geolocation) {
-
-
-
-        navigator.geolocation.getCurrentPosition(
-
-            function(position) {
-
-
-
-                var lat = position.coords.latitude;
-
-                var lon = position.coords.longitude;
+var iconeEvento = L.icon({
+iconUrl:"https://cdn-icons-png.flaticon.com/512/684/684908.png",
+iconSize:[32,32],
+iconAnchor:[16,32]
+});
 
 
+// eventos
 
-                // Centralizar mapa na posição do usuário
+var eventos = <?php echo json_encode($eventos); ?>;
 
-                map.setView([lat, lon], 15);
+var grupoEventos = [];
 
+eventos.forEach(function(evento){
 
+if(evento.latitude && evento.longitude){
 
-                // Adicionar marcador
+    var marker = L.marker(
+    [evento.latitude,evento.longitude],
+    {icon:iconeEvento}
+)
+.addTo(map)
+.bindPopup(
+"<b>"+evento.descricao+"</b><br>"+
+"📍 "+evento.rua+", "+evento.numero+"<br>"+
+evento.bairro
+);
 
-                L.marker([lat, lon])
+grupoEventos.push(marker);
 
-                    .addTo(map)
+}
 
-                    .bindPopup("📍 Você está aqui!")
-
-                    .openPopup();
-
-
-
-            },
-
-            function(error) {
-
-                alert("Não foi possível obter sua localização.");
-
-            }
-
-        );
+});
 
 
+// Zoom automático
 
-    } else {
+if(grupoEventos.length > 0){
 
-        alert("Seu navegador não suporta geolocalização.");
+    var grupo = L.featureGroup(grupoEventos);
 
-    }
+    setTimeout(function(){
 
-</script>
-<div id="loginModal" class="modal">
-    <div class="modal-conteudo">
-        <span class="fechar">&times;</span>
-        </div>
-</div>
+    map.fitBounds(grupo.getBounds());
 
-<script>
-    // Script básico para abrir a modal
-    const modal = document.getElementById("loginModal");
-    const btn = document.getElementById("abrirModal");
-    const span = document.getElementsByClassName("fechar")[0];
+    },1000);
 
-    if (btn) {
-        btn.onclick = function() { modal.style.display = "block"; }
-    }
-    if (span) {
-        span.onclick = function() { modal.style.display = "none"; }
-    }
-    window.onclick = function(event) {
-        if (event.target == modal) { modal.style.display = "none"; }
-    }
+}
+
+
+//geolocalização
+
+var marcadorUsuario = null;
+var circuloUsuario = null;
+var minhaLat = null;
+var minhaLon = null;
+
+function localizarUsuario(){
+
+if(!navigator.geolocation){
+
+    alert("Geolocalização não suportada");
+    return;
+
+}
+
+navigator.geolocation.getCurrentPosition(
+
+function(pos){
+
+minhaLat = pos.coords.latitude;
+minhaLon = pos.coords.longitude;
+
+console.log("Minha localização:",minhaLat,minhaLon);
+
+// remove marcador antigo 
+
+if(marcadorUsuario){
+    map.removeLayer(marcadorUsuario);
+}
+
+if(circuloUsuario){
+    map.removeLayer(circuloUsuario);
+}
+
+// marcador 
+
+marcadorUsuario = L.marker(
+[minhaLat,minhaLon],
+{icon:iconeUsuario}
+).addTo(map)
+.bindPopup("📍 Você está aqui");
+
+// círculo de precisão 
+
+circuloUsuario = L.circle(
+[minhaLat,minhaLon],
+{
+radius:50,
+color:"#0078ff",
+fillColor:"#0078ff",
+fillOpacity:0.2
+}
+).addTo(map);
+
+// centraliza 
+
+map.setView([minhaLat,minhaLon],15);
+
+},
+
+function(error){
+
+console.log(error);
+
+if(error.code===1){
+    alert("Permissão de localização negada.");
+}
+else if(error.code===2){
+    alert("Localização indisponível.");
+}
+else{
+    alert("Tempo excedido ao localizar.");
+}
+
+},
+
+{
+enableHighAccuracy:true,
+timeout:15000,
+maximumAge:0
+}
+
+);
+
+}
+
+// iniciar localização 
+
+localizarUsuario();
+
+
+//BOTÃO LOCALIZAÇÃO 
+
+document.getElementById("btnLocalizacao").onclick=function(){
+
+if(minhaLat && minhaLon){
+    map.setView([minhaLat,minhaLon],16);
+}else{
+    localizarUsuario();
+}
+
+};
+
 </script>
 
 </body>
-
 </html>
-
